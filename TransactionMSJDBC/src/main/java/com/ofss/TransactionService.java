cc2b5ed43934d10f20f92da12cde889ddff65d2e
Pradeep1G <98323512+Pradeep1G@users.noreply.github.com>
2025-09-02 14:26:07 +0530

    Update TransactionService.java

--- TransactionMSJDBC/src/main/java/com/ofss/TransactionService.java
+++ TransactionMSJDBC/src/main/java/com/ofss/TransactionService.java
@@ -3,19 +3,26 @@ package com.ofss;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.ofss.exception.TransactionException;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 @Service
 public class TransactionService {
+
+    private static final Logger logger = LoggerFactory.getLogger(TransactionService.class);
 
     @Autowired
     private TransactionDAO transactionDAO;
 
-    public boolean performTransaction(Transaction t) {
+    public void performTransaction(Transaction t) throws TransactionException {
         try {
             transactionDAO.saveTransaction(t);
-            return true;
-        } catch(Exception e) {
-            // Optionally log exception
-            return false;
+            logger.info("Transaction saved for customer: {}", t.getCustomerId());
+        } catch (Exception e) {
+            logger.error("Error performing transaction for customer: {}", t.getCustomerId(), e);
+            throw new TransactionException("Transaction persistence failed", e);
         }
     }
 }
