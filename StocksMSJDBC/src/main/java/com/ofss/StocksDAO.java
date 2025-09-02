5230925620c0625f7bd8ca3f0603391359bad7e4
Pradeep1G <98323512+Pradeep1G@users.noreply.github.com>
2025-09-02 14:19:08 +0530

    Update StocksDAO.java

--- StocksMSJDBC/src/main/java/com/ofss/StocksDAO.java
+++ StocksMSJDBC/src/main/java/com/ofss/StocksDAO.java
@@ -1,20 +1,20 @@
 package com.ofss;
-
-import java.sql.Date;
 import java.util.ArrayList;
 import java.util.List;
 import javax.sql.DataSource;
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
 import org.springframework.jdbc.core.support.JdbcDaoSupport;
 import org.springframework.stereotype.Repository;
 import jakarta.annotation.PostConstruct;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @Repository
 public class StocksDAO extends JdbcDaoSupport {
+
+    private static final Logger logger = LoggerFactory.getLogger(StocksDAO.class);
 
     @Autowired
     DataSource dataSource;
@@ -22,50 +22,47 @@ public class StocksDAO extends JdbcDaoSupport {
     @PostConstruct
     public void init() {
         setDataSource(dataSource);
-        System.out.println("Stock DAO initialized with datasource " + dataSource);
-    }
-
-    public JdbcTemplate giveJdbcTemplate() {
-        return getJdbcTemplate();
     }
 
     public void addAStock(Stocks s) {
         String sql = "INSERT INTO stock (STOCK_ID, STOCK_NAME, STOCK_PRICE, STOCK_VOLUME, LISTED_PRICE, LISTED_DATE, LISTED_EXCHANGE) VALUES (?,?,?,?,?,?,?)";
-        giveJdbcTemplate().update(sql, s.getStockId(), s.getStockName(), s.getStockPrice(), s.getStockVolume(),
+        getJdbcTemplate().update(sql, s.getStockId(), s.getStockName(), s.getStockPrice(), s.getStockVolume(),
                 s.getListedPrice(), s.getListedDate(), s.getListedExchange());
+        logger.debug("Stock inserted: {}", s.getStockId());
     }
 
-    public ArrayList<Stocks> getAllStocks() {
+    public List<Stocks> getAllStocks() {
         String sql = "SELECT * FROM stock";
-        List<Stocks> stocks = giveJdbcTemplate().query(sql, stockRowMapper);
-        return new ArrayList<>(stocks);
+        List<Stocks> stocks = getJdbcTemplate().query(sql, stockRowMapper);
+        logger.debug("Fetched {} stocks", stocks.size());
+        return stocks;
     }
 
-    public Stocks getStockById(int id) {
+    public Stocks getStockById(long id) {
         String sql = "SELECT * FROM stock WHERE STOCK_ID = ?";
-        return giveJdbcTemplate().queryForObject(sql, stockRowMapper, id);
-    }
-
-    public ResponseEntity<Object> deleteAStockById(int sid) {
-        int rowsAffected = giveJdbcTemplate().update("DELETE FROM stock WHERE STOCK_ID = ?", sid);
-        if (rowsAffected > 0)
-            return ResponseEntity.status(200).body("Stock deleted successfully");
-        else
-            return ResponseEntity.status(404).body("Stock not found");
-    }
-
-    public ResponseEntity<Object> updateStock(int sid, Stocks s) {
-        String sql = "UPDATE stock SET STOCK_NAME = ?, STOCK_PRICE = ?, STOCK_VOLUME = ?, LISTED_PRICE = ?, LISTED_DATE = ?, LISTED_EXCHANGE = ? WHERE STOCK_ID = ?";
-        int rowsAffected = giveJdbcTemplate().update(sql, s.getStockName(), s.getStockPrice(), s.getStockVolume(),
-                s.getListedPrice(), s.getListedDate(), s.getListedExchange(), sid);
-        if (rowsAffected > 0) {
-            return ResponseEntity.status(200).body("Stock updated successfully");
-        } else {
-            return ResponseEntity.status(404).body("Stock not found");
+        List<Stocks> result = getJdbcTemplate().query(sql, stockRowMapper, id);
+        if (result.isEmpty()) {
+            logger.info("Stock with ID {} not found", id);
+            return null;
         }
+        return result.get(0);
     }
 
-    public ResponseEntity<Object> patchStock(int sid, Stocks s) {
+    public boolean deleteAStockById(long sid) {
+        int rowsAffected = getJdbcTemplate().update("DELETE FROM stock WHERE STOCK_ID = ?", sid);
+        logger.debug("Delete stock {} rows affected: {}", sid, rowsAffected);
+        return rowsAffected > 0;
+    }
+
+    public boolean updateStock(long sid, Stocks s) {
+        String sql = "UPDATE stock SET STOCK_NAME = ?, STOCK_PRICE = ?, STOCK_VOLUME = ?, LISTED_PRICE = ?, LISTED_DATE = ?, LISTED_EXCHANGE = ? WHERE STOCK_ID = ?";
+        int rowsAffected = getJdbcTemplate().update(sql, s.getStockName(), s.getStockPrice(), s.getStockVolume(),
+                s.getListedPrice(), s.getListedDate(), s.getListedExchange(), sid);
+        logger.debug("Update stock {} rows affected: {}", sid, rowsAffected);
+        return rowsAffected > 0;
+    }
+
+    public boolean patchStock(long sid, Stocks s) {
         StringBuilder sql = new StringBuilder("UPDATE stock SET ");
         List<Object> params = new ArrayList<>();
         if (s.getStockName() != null) {
@@ -93,19 +90,15 @@ public class StocksDAO extends JdbcDaoSupport {
             params.add(s.getListedExchange());
         }
         if (params.isEmpty()) {
-            return ResponseEntity.status(400).body("No data provided to update");
+            logger.warn("No data provided for patching stock {}", sid);
+            return false;
         }
-        // Remove trailing comma and space
         sql.setLength(sql.length() - 2);
         sql.append(" WHERE STOCK_ID = ?");
         params.add(sid);
-
-        int rowsAffected = giveJdbcTemplate().update(sql.toString(), params.toArray());
-        if (rowsAffected > 0) {
-            return ResponseEntity.status(200).body("Stock patched successfully");
-        } else {
-            return ResponseEntity.status(404).body("Stock not found");
-        }
+        int rowsAffected = getJdbcTemplate().update(sql.toString(), params.toArray());
+        logger.debug("Patch stock {} rows affected: {}", sid, rowsAffected);
+        return rowsAffected > 0;
     }
 
     // RowMapper for Stocks
