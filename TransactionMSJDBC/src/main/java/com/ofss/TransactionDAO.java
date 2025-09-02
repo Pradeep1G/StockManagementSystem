1eb6ee3f8e5ffd90fc6c98ca95fd3ff53b060c6a
Pradeep1G <98323512+Pradeep1G@users.noreply.github.com>
2025-09-02 14:25:40 +0530

    Update TransactionDAO.java

--- TransactionMSJDBC/src/main/java/com/ofss/TransactionDAO.java
+++ TransactionMSJDBC/src/main/java/com/ofss/TransactionDAO.java
@@ -2,15 +2,18 @@ package com.ofss;
 
 import org.springframework.jdbc.core.support.JdbcDaoSupport;
 import org.springframework.stereotype.Repository;
-
 import jakarta.annotation.PostConstruct;
 import javax.sql.DataSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import java.sql.Timestamp;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @Repository
 public class TransactionDAO extends JdbcDaoSupport {
+
+    private static final Logger logger = LoggerFactory.getLogger(TransactionDAO.class);
 
     @Autowired
     private DataSource dataSource;
@@ -19,27 +22,18 @@ public class TransactionDAO extends JdbcDaoSupport {
     private void init() {
         setDataSource(dataSource);
     }
-    public JdbcTemplate createJdbcTemplate() {
-        return getJdbcTemplate();
-    }
-    public void saveTransaction(Transaction t) {
-        // 1. Get next transaction_id from sequence
-        String getIdSQL = "SELECT transaction_seq.NEXTVAL FROM dual";
-        Long transactionId = createJdbcTemplate().queryForObject(getIdSQL, Long.class);
 
-        // 2. Insert into transactions table
-        String insertSQL = "INSERT INTO transactions "
-            + "(transaction_id, stock_id, customer_id, transaction_type, transaction_price, volume, transaction_time) "
-            + "VALUES (?,?,?,?,?,?,?)";
-        createJdbcTemplate().update(
-            insertSQL,
-            transactionId,
-            t.getStockId(),
-            t.getCustomerId(),
-            t.getTransactionType(),
-            t.getTransactionPrice(),
-            t.getVolume(),
-            (t.getTransactionTime() != null) ? Timestamp.valueOf(t.getTransactionTime()) : new Timestamp(System.currentTimeMillis())
-        );
+    public void saveTransaction(Transaction t) {
+        String getIdSQL = "SELECT transaction_seq.NEXTVAL FROM dual";
+        JdbcTemplate jdbcTemplate = getJdbcTemplate();
+        if (jdbcTemplate == null) {
+            logger.error("JdbcTemplate is not initialized");
+            throw new IllegalStateException("JdbcTemplate is not initialized");
+        }
+        Long transactionId = jdbcTemplate.queryForObject(getIdSQL, Long.class);
+        String insertSQL = "INSERT INTO transactions (transaction_id, stock_id, customer_id, transaction_type, transaction_price, volume, transaction_time) VALUES (?,?,?,?,?,?,?)";
+        Timestamp txTime = (t.getTransactionTime() != null) ? Timestamp.valueOf(t.getTransactionTime()) : new Timestamp(System.currentTimeMillis());
+        jdbcTemplate.update(insertSQL, transactionId, t.getStockId(), t.getCustomerId(), t.getTransactionType(), t.getTransactionPrice(), t.getVolume(), txTime);
+        logger.debug("Transaction inserted: {} for stock: {}", transactionId, t.getStockId());
     }
 }
