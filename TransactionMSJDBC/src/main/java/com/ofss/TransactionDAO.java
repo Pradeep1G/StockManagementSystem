ee680e53b94767ca9f3e429928e625648f829acb
Pradeep1G <98323512+Pradeep1G@users.noreply.github.com>
2025-09-05 15:28:48 +0530

    Update TransactionDAO.java

--- TransactionMSJDBC/src/main/java/com/ofss/TransactionDAO.java
+++ TransactionMSJDBC/src/main/java/com/ofss/TransactionDAO.java
@@ -30,10 +30,44 @@ public class TransactionDAO extends JdbcDaoSupport {
             logger.error("JdbcTemplate is not initialized");
             throw new IllegalStateException("JdbcTemplate is not initialized");
         }
+        
         Long transactionId = jdbcTemplate.queryForObject(getIdSQL, Long.class);
         String insertSQL = "INSERT INTO transactions (transaction_id, stock_id, customer_id, transaction_type, transaction_price, volume, transaction_time) VALUES (?,?,?,?,?,?,?)";
         Timestamp txTime = (t.getTransactionTime() != null) ? Timestamp.valueOf(t.getTransactionTime()) : new Timestamp(System.currentTimeMillis());
         jdbcTemplate.update(insertSQL, transactionId, t.getStockId(), t.getCustomerId(), t.getTransactionType(), t.getTransactionPrice(), t.getVolume(), txTime);
+        
+        
+        if(t.getTransactionType().equals("SELL")) {
+        	increaseVolume(t.getVolume(), t.getStockId());
+        } else {
+        	decreaseVolume(t.getVolume(), t.getStockId());
+        }
+        
+        
         logger.debug("Transaction inserted: {} for stock: {}", transactionId, t.getStockId());
     }
+
+    private void decreaseVolume(Integer volume, Long stockId) {
+        String updateSQL = "UPDATE stock SET stock_volume = stock_volume - ? WHERE stock_id = ?";
+        JdbcTemplate jdbcTemplate = getJdbcTemplate();
+        if (jdbcTemplate == null) {
+            logger.error("JdbcTemplate is not initialized");
+            throw new IllegalStateException("JdbcTemplate is not initialized");
+        }
+        jdbcTemplate.update(updateSQL, volume, stockId);
+        logger.debug("Decreased stock {} volume by {}", stockId, volume);
+    }
+
+    private void increaseVolume(Integer volume, Long stockId) {
+        String updateSQL = "UPDATE stock SET stock_volume = stock_volume + ? WHERE stock_id = ?";
+        JdbcTemplate jdbcTemplate = getJdbcTemplate();
+        if (jdbcTemplate == null) {
+            logger.error("JdbcTemplate is not initialized");
+            throw new IllegalStateException("JdbcTemplate is not initialized");
+        }
+        jdbcTemplate.update(updateSQL, volume, stockId);
+        logger.debug("Increased stock {} volume by {}", stockId, volume);
+    }
+    
+    
 }
