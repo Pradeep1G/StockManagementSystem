3ea382def39963a981a3304a20770571af041608
Pradeep1G <98323512+Pradeep1G@users.noreply.github.com>
2025-09-08 09:55:47 +0530

    Update TransactionDAO.java

--- TransactionMSJDBC/src/main/java/com/ofss/TransactionDAO.java
+++ TransactionMSJDBC/src/main/java/com/ofss/TransactionDAO.java
@@ -7,6 +7,8 @@ import javax.sql.DataSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import java.sql.Timestamp;
+import java.util.List;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -35,19 +37,11 @@ public class TransactionDAO extends JdbcDaoSupport {
         String insertSQL = "INSERT INTO transactions (transaction_id, stock_id, customer_id, transaction_type, transaction_price, volume, transaction_time) VALUES (?,?,?,?,?,?,?)";
         Timestamp txTime = (t.getTransactionTime() != null) ? Timestamp.valueOf(t.getTransactionTime()) : new Timestamp(System.currentTimeMillis());
         jdbcTemplate.update(insertSQL, transactionId, t.getStockId(), t.getCustomerId(), t.getTransactionType(), t.getTransactionPrice(), t.getVolume(), txTime);
-        
-        
-        if(t.getTransactionType().equals("SELL")) {
-        	increaseVolume(t.getVolume(), t.getStockId());
-        } else {
-        	decreaseVolume(t.getVolume(), t.getStockId());
-        }
-        
-        
+         
         logger.debug("Transaction inserted: {} for stock: {}", transactionId, t.getStockId());
     }
 
-    private void decreaseVolume(Integer volume, Long stockId) {
+    public void decreaseVolume(Integer volume, Long stockId) {
         String updateSQL = "UPDATE stock SET stock_volume = stock_volume - ? WHERE stock_id = ?";
         JdbcTemplate jdbcTemplate = getJdbcTemplate();
         if (jdbcTemplate == null) {
@@ -58,7 +52,7 @@ public class TransactionDAO extends JdbcDaoSupport {
         logger.debug("Decreased stock {} volume by {}", stockId, volume);
     }
 
-    private void increaseVolume(Integer volume, Long stockId) {
+    public void increaseVolume(Integer volume, Long stockId) {
         String updateSQL = "UPDATE stock SET stock_volume = stock_volume + ? WHERE stock_id = ?";
         JdbcTemplate jdbcTemplate = getJdbcTemplate();
         if (jdbcTemplate == null) {
@@ -68,6 +62,25 @@ public class TransactionDAO extends JdbcDaoSupport {
         jdbcTemplate.update(updateSQL, volume, stockId);
         logger.debug("Increased stock {} volume by {}", stockId, volume);
     }
+    
+    public void updateCustomerProfit(Long customerId, Double profitToAdd) {
+        String sql = "UPDATE customers SET profit = NVL(profit,0) + ? WHERE customer_id = ?";
+        JdbcTemplate jdbcTemplate = getJdbcTemplate();
+        jdbcTemplate.update(sql, profitToAdd, customerId);
+    }
+
+	public List<Transaction> getTransactions(Long customerId, Long stockId) {
+		String sql = "SELECT * FROM transactions WHERE customer_id=? AND stock_id=? ORDER BY stock_id";
+		return getJdbcTemplate().query(sql, (rs, rowNum) -> {
+            Transaction t = new Transaction();
+            t.setStockId(rs.getLong("stock_id"));
+            t.setTransactionType(rs.getString("transaction_type"));
+            t.setTransactionPrice(rs.getDouble("transaction_price"));
+            t.setVolume(rs.getInt("volume"));
+            t.setTransactionTime(rs.getTimestamp("transaction_time").toLocalDateTime());
+            return t;
+        }, customerId, stockId);  
+	}
     
     
 }
