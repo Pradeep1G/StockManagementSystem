c026cb19d0a883550229d3930c752d7754c5a3f4
Pradeep1G <98323512+Pradeep1G@users.noreply.github.com>
2025-09-08 09:56:12 +0530

    Update TransactionService.java

--- TransactionMSJDBC/src/main/java/com/ofss/TransactionService.java
+++ TransactionMSJDBC/src/main/java/com/ofss/TransactionService.java
@@ -5,6 +5,8 @@ import org.springframework.stereotype.Service;
 
 import com.ofss.exception.TransactionException;
 
+import java.util.List;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -17,6 +19,19 @@ public class TransactionService {
     private TransactionDAO transactionDAO;
 
     public void performTransaction(Transaction t) throws TransactionException {
+    	logger.info("Transaction is of type selling {}",t.getTransactionType());
+    	try {
+    		if("SELL".equalsIgnoreCase(t.getTransactionType())) {
+    			logger.info("Transaction is of type selling ");
+            	transactionDAO.increaseVolume(t.getVolume(), t.getStockId());
+            	transactionDAO.updateCustomerProfit(t.getCustomerId(), calculateNetProfit(t));
+            } else {
+            	transactionDAO.decreaseVolume(t.getVolume(), t.getStockId());
+            }
+        } catch (Exception e) {
+            logger.error("Error performing transaction for customer: {}", t.getCustomerId(), e);
+            throw new TransactionException("Transaction persistence failed", e);
+        }
         try {
             transactionDAO.saveTransaction(t);
             logger.info("Transaction saved for customer: {}", t.getCustomerId());
@@ -25,4 +40,11 @@ public class TransactionService {
             throw new TransactionException("Transaction persistence failed", e);
         }
     }
+
+	private Double calculateNetProfit(Transaction t) {
+		// TODO Auto-generated method stub
+		List<Transaction> txns = transactionDAO.getTransactions(t.getCustomerId(), t.getStockId());
+		logger.info("Transaction is of type selling ");
+		return ProfitCalculator.calculateRealizedProfit(txns, t);
+	}
 }
